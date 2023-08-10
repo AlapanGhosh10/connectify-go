d672c9b77cd72f7003bee2433b51140626cc577d
AlapanGhosh10 <alapanzico10@gmail.com>
2023-08-10 21:38:04 +0530

    setup done

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+function Header() {
+    return (
+        <header>
+            <h1>Connectify</h1>
+            <input id="main-search" type="text" />
+            <button>Search</button>
+            <div>
+                <button>SignIn</button>
+                <button>SignUp</button>
+            </div>
+        </header>
+    )
+}
+
+export default Header;
