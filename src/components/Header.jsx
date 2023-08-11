dad01e5a19273321c1405fb4861193fa3c458b0d
AlapanGhosh10 <alapanzico10@gmail.com>
2023-08-11 23:30:45 +0530

    sidebar started

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,15 @@
+import './Header.css'
+
 function Header() {
     return (
         <header>
-            <h1>Connectify</h1>
-            <input id="main-search" type="text" />
-            <button>Search</button>
+            <div id='main-header'>
+                <h1>Connectify</h1>
+                <div>
+                    <input id="main-search" type="text" />
+                    <button>Search</button>
+                </div>
+            </div>
             <div>
                 <button>SignIn</button>
                 <button>SignUp</button>
---
+++ src/components/Left.jsx
@@ -0,0 +1,13 @@
+import '../Left.css'
+
+function Left() {
+    return (
+        <div id="left-sidebar">
+            <ul>
+                # List here
+            </ul>
+        </div>
+    )
+}
+
+export default Left;
